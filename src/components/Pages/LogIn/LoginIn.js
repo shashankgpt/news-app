f974476e88ab62b669f056cbe44e6519a2b89854
shashank <shashankgpt270@gmail.com>
2020-12-12 05:27:15 -0800

    form added

--- src/components/Pages/LogIn/LoginIn.js
+++ src/components/Pages/LogIn/LoginIn.js
@@ -1,12 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 import PageWrapper from "../../common/PageWrapper/PageWrapper";
+import "./login.css";
 
-function LoginIn() {
-    return (
-        <PageWrapper>
-            Login
-        </PageWrapper>
-    )
+function SignUp() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+  return (
+    <PageWrapper>
+      <form className="text-left form-margin" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="username">
+            Username
+            <input
+              type="text"
+              className={`form-control ${ errors.username ? 'is-invalid' :''}`}
+              name="username"
+              id="username"
+              aria-describedby="emailHelp"
+              placeholder="Enter username"
+              ref={register({ required: true })}
+            />
+          </label>
+          {errors.username && (
+            <small id="usernameHelp" className="form-text text-danger">
+              Username is required
+            </small>
+          )}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              className={`form-control ${ errors.password ? 'is-invalid' :''}`}
+              name="password"
+              id="password"
+              placeholder="Password"
+              ref={register({
+                required: "You must specify a password",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters"
+                }
+              })}
+            />
+          </label>
+          {errors.password && (
+            <small id="usernameHelp" className="form-text text-danger">
+             {errors.password.message}
+            </small>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </PageWrapper>
+  );
 }
 
-export default LoginIn
+export default SignUp;
--- src/components/Pages/SignUp/SignUp.js
+++ src/components/Pages/SignUp/SignUp.js
@@ -1,12 +1,87 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 import PageWrapper from "../../common/PageWrapper/PageWrapper";
+import "./sign-up.css";
 
 function SignUp() {
-    return (
-        <PageWrapper>
-            <h1>Sign Up</h1>
-        </PageWrapper>
-    )
+  const { register, handleSubmit,watch, errors } = useForm();
+  const password = React.useRef({});
+  const onSubmit = (data) => console.log(data);
+  password.current = watch("password", "");
+  console.log(errors);
+  return (
+    <PageWrapper>
+      <form className="text-left form-margin" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label htmlFor="username">
+            Username
+            <input
+              type="text"
+              className={`form-control ${ errors.username ? 'is-invalid' :''}`}
+              name="username"
+              id="username"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              ref={register({ required: true })}
+            />
+          </label>
+          {errors.username && (
+            <small id="usernameHelp" className="form-text text-danger">
+              Username is required
+            </small>
+          )}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">
+            Password
+            <input
+              type="password"
+              className={`form-control ${ errors.password ? 'is-invalid' :''}`}
+              name="password"
+              id="password"
+              placeholder="Password"
+              ref={register({
+                required: "You must specify a password",
+                minLength: {
+                  value: 8,
+                  message: "Password must have at least 8 characters"
+                }
+              })}
+            />
+          </label>
+          {errors.password && (
+            <small id="usernameHelp" className="form-text text-danger">
+             {errors.password.message}
+            </small>
+          )}
+        </div>
+        <div className="form-group">
+          <label htmlFor="cpassword">
+            Confirm Password
+            <input
+              type="password"
+              className={`form-control ${ errors.cpassword ? 'is-invalid' :''}`}
+              name="cpassword"
+              id="cpassword"
+              placeholder="Password"
+              ref={register({
+                validate: value =>
+                  value === password.current || "The passwords do not match"
+              })}
+            />
+          </label>
+          {errors.cpassword && (
+            <small id="usernameHelp" className="form-text text-danger">
+             {errors.cpassword.message}
+            </small>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </PageWrapper>
+  );
 }
 
-export default SignUp
+export default SignUp;
